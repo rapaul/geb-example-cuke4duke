86c1d2e8acc30a4a1d5a2328c65a329bb76f96f5
Luke Daley <ld@ldaley.com>
2010-08-29 22:12:22 +1000

    Added some notes on how reporting would work if cuke4duke exposed the scenario name.

--- features/support/env.groovy
+++ features/support/env.groovy
@@ -1,7 +1,19 @@
 import geb.Browser
+import geb.report.PageSourceReporter
 
 this.metaClass.mixin(cuke4duke.GroovyDsl)
 
-Before() {
-  browser = new Browser()
+Before() { scenario ->
+	browser = new Browser()
+}
+
+After() { scenario ->
+	// cuke4duke doesn't provide a way to get the name of the scenario.
+	// If it did, you could add reporting (i.e. dumping the page source)
+	// by the following code.
+	
+	// def reportsDir = new File("target/geb-output")
+	// def reportName = scenario.name
+	
+	// new PageSourceReporter(reportsDir).writeReport(reportName, browser)
 }
